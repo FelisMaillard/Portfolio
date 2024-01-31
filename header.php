4684987664fdf5c55953d64ea480a082a768ce4d
Félis Maillard <151616377+FelisMaillard@users.noreply.github.com>
2024-01-31 14:32:40 +0100

    oui

---
+++ header.php
@@ -0,0 +1,11 @@
+<div class="h-container">
+    <div class="h-nav-container">
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="parcour.html">Parcours</a>
+            <a href="aproposdemoi.html">A propos</a>
+            <a href="img/CVFélisMaillard.pdf"
+            download="img/CVFélisMaillard.pdf" target="_blank">Télécharger mon CV</a>
+        </nav>
+    </div>
+</div>  
--- index.php
+++ index.php
@@ -10,17 +10,9 @@
     <title>Portfolio Félis Maillard</title>
 </head>
 <header>
-    <div class="h-container">
-        <div class="h-nav-container">
-            <nav>
-                <a href="index.html">Home</a>
-                <a href="parcour.html">Parcours</a>
-                <a href="aproposdemoi.html">A propos</a>
-                <a href="img/CVFélisMaillard.pdf"
-                download="img/CVFélisMaillard.pdf" target="_blank">Télécharger mon CV</a>
-            </nav>
-        </div>
-    </div>  
+    <?php
+    include 'header.php'
+    ?>
 </header>
 <body>
     <div class="body-container">
